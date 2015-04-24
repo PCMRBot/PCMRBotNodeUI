5a44df5e6ff5e147fcf3bbcce68e29800f165525
James Wolff <james.wolff0@gmail.com>
2015-04-24 18:03:06 -0500

    Initial Commit

---
+++ app/assets/js/app.js
@@ -0,0 +1,58 @@
+var app = {};
+
+app.node = {};
+app.node.gui = require('nw.gui');
+app.node.win = app.node.gui.Window.get();
+app.node.fs = require('fs');
+app.node.events = require('events');
+app.node.os = require('os');
+app.node.open = require('open');
+app.node.win.isMaximized = false;
+
+app.node.win.on('maximize', function() {
+    app.node.win.isMaximized = true;
+});
+
+app.node.win.on('unmaximize', function() {
+    app.node.win.isMaximized = false;
+});
+
+app.init = function() {
+	app.node.gui.Window.get().moveTo(0,0);
+}
+	
+app.minimize = function() {
+	app.node.win.minimize();
+}
+
+app.close = function() {
+	app.node.win.close();
+}
+
+app.maximize = function() {
+	app.node.win.maximize();
+}
+
+app.unmaximize = function() {
+	app.node.win.unmaximize();
+}
+
+app.isMaximized = function() {
+	return app.node.win.isMaximized;
+}
+
+app.openDevConsole = function() {
+	app.node.win.showDevTools();
+}
+
+app.openExternalPage = function(page) {
+	app.node.gui.Shell.openExternal(page);
+}
+
+/*$('.external_link').on('click', function(){
+	app.node.open(this.href, {
+		"nodejs": false,
+		"new-instance": false
+	});
+	return false;
+});*/
